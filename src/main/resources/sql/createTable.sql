a07df7c79981320b98e7bbb5dd10e0edad48a617
like-company <476956004@qq.com>
2018-11-07 15:02:20 +0800

    增加建表语句

---
+++ src/main/resources/sql/createTable.sql
@@ -0,0 +1,13 @@
+create database how2java;
+
+use how2java;
+CREATE TABLE category_ (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  name varchar(30),
+  PRIMARY KEY (id)
+) DEFAULT CHARSET=UTF8;
+
+insert into category_ values(null,'category 1');
+insert into category_ values(null,'category 2');
+insert into category_ values(null,'category 3');
+insert into category_ values(null,'category 4');
