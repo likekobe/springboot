293336af06b515f823f0c77980e17524aa64050b
like-company <476956004@qq.com>
2018-11-07 15:05:01 +0800

    增加建表语句

--- src/main/resources/sql/createTable.sql
+++ src/main/resources/sql/createTable.sql
@@ -11,3 +11,4 @@ insert into category_ values(null,'category 1');
 insert into category_ values(null,'category 2');
 insert into category_ values(null,'category 3');
 insert into category_ values(null,'category 4');
+insert into category_ values(null,'category 5');
